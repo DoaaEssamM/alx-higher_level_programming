73d02455db2734393968020a30f71cf77b596539
DoaaEssamM <doaaessamm3@gmail.com>
2023-10-31 12:09:24 -0700

    dn

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,12 +1,4 @@
 #!/user/bin/python3
-#!/usr/bin/env python3
-print_last_digit = __import__('9-print_last_digit').print_last_digit
-
-print_last_digit(98)
-print_last_digit(0)
-r = print_last_digit(-1024)
-print(r)
-
 def print_last_digit(number):
     number = abs(number) % 10
     print(number, end="")
