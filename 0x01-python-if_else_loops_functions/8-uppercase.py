3e1ba8e25c6634383bd843e5beb967282464e018
DoaaEssamM <doaaessamm3@gmail.com>
2023-10-31 12:14:15 -0700

    dn

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
-#!/usr/bin/env python3
-uppercase = __import__('8-uppercase').uppercase
-
-uppercase("best")
-uppercase("Best School 98 Battery street")
 def uppercase(str):
     for i in str:
         if ord("a") <= ord(i) <= ord("z"):
