d35343fdc2f128036e3149a6cd0b75fa34618c8e
DoaaEssamM <doaaessamm3@gmail.com>
2023-11-02 14:05:25 -0700

    dn

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,6 +3,6 @@ if __name__ == "__main__":
     """print all hidden directories"""
     import hidden_4
 
-    for i in dir(hidden)
-    if i[:] != "__":
-        print(i)       
+    for i in dir(hidden):
+        if i[:] != "__":
+            print(i)       
